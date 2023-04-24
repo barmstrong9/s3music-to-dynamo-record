92cb8c2eaf44012ea4027bfaf3e7e464be04535b
Brandon Armstrong <brandonarmstrong@Brandons-MBP.localdomain>
2023-04-24 14:48:45 +0100

    added a buildspec and cfntemplate

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"log"
+	"context"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/events"
 )
 
 func main() {
 	lambda.Start(Handler)
 }
 
-func Handler() {
-	log.Println("Testing")
+func Handler(ctx context.Context, s3Event events.S3Event) {
+	for _, record := range s3Event.Records {
+		s3 := record.S3
+		log.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3.Bucket.Name, s3.Object.Key)
+}
 }
 
 //Remember to do
